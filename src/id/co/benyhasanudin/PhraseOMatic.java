9af1ceb980eab1bb6e5b8bd902d957d634d70935
Beny Hasanudin <bhasanudin@gmail.com>
2019-11-11 16:04:24 +0700

    Typo in operator it should be * instead of +

--- src/id/co/benyhasanudin/PhraseOMatic.java
+++ src/id/co/benyhasanudin/PhraseOMatic.java
@@ -4,9 +4,10 @@ public class PhraseOMatic {
 
 
     public static void main(String[] args) {
-        String[] wordListOne = {"24/7",
+        String[] wordListOne = {
+                "24/7",
                 "multi-Tier",
-                "30,000",
+                "30,000 feet",
                 "B-to-B",
                 "win-win",
                 "front-end",
@@ -36,7 +37,7 @@ public class PhraseOMatic {
                 "cooperative",
                 "accelerated"
         };
-        String [] wordListThree = {
+        String[] wordListThree = {
                 "process",
                 "tipping-point",
                 "solution",
@@ -54,9 +55,9 @@ public class PhraseOMatic {
         int twoLength = wordListTwo.length;
         int threeLength = wordListThree.length;
 
-        int rand1 = (int) (Math.random() + oneLength);
-        int rand2 = (int) (Math.random() + twoLength);
-        int rand3 = (int) (Math.random()+threeLength);
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
 
         String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
         System.out.println("What we need is a " + phrase);
