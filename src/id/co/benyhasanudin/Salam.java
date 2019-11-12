9a794030bfb5abc3338bb5281128d56c2fb11de4
Beny Hasanudin <bhasanudin@gmail.com>
2019-11-12 08:23:33 +0700

    Added Salam App

---
+++ src/id/co/benyhasanudin/Salam.java
@@ -0,0 +1,7 @@
+package id.co.benyhasanudin;
+
+public class Salam {
+    public static void main(String[] args) {
+        System.out.println("Assalamualaikum! Pak Tito yang baik hati dan tidak sombong");
+    }
+}
